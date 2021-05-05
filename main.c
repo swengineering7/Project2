2f2884bdff4f0712433a22744e3b3bd66250421d
Choi <cjw1359@gmail.com>
2021-05-05 10:15:02 -0700

    function 5, 6

--- main.c
+++ main.c
@@ -5,7 +5,7 @@
 #include <time.h>
 
 struct stat stat1, stat2;
-struct tm *time1, *time2;
+struct tm time1, time2;
 
 void filestat1();
 void filestat2();
@@ -39,23 +39,46 @@ void filestat2(){
 
 //파일 1의 시간 정보를 가져오는 함수 작성
 void filetime1(){
-    time1 = localtime(&stat1.st_mtime); 
+    time1 = *localtime(&stat1.st_mtime); 
 }
 
 //파일 2의 시간 정보를 가져오는 함수 작성
 void filetime2(){
-    time2 = localtime(&stat2.st_mtime); 
+    time2 = *localtime(&stat2.st_mtime); 
 }
 
 //두 개의 파일 크기를 비교하는 함수 작성
 void sizecmp(){
 
+        printf("size compare\n");
+        if ((int)stat1.st_size > (int)stat2.st_size) {
+                printf("text1 is bigger\n");
+        }
+        else if ((int)stat1.st_size < (int)stat2.st_size) {
+                printf("text2 is bigger\n");
+        }
+        else
+                printf("sizes are equal\n");
+	
+	return;
 }
 
 //두 개의 파일 블락 수를 비교하는 함수 작성
 void blockcmp(){
-    
+
+        printf("block compare\n");
+        if ((int)stat1.st_blocks > (int)stat2.st_blocks) {
+                printf("text1 is bigger\n");
+        }
+        else if ((int)stat1.st_blocks < (int)stat2.st_blocks) {
+                printf("text2 is bigger\n");
+        }
+        else
+                printf("sizes are equal\n");
+	
+	return;
 }
+
 
 //두 개의 파일 수정 날짜를 비교하는 함수 작성
 void datecmp(){
